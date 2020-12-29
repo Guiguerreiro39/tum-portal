ce60fca5d8db6640581d2f07d72f6973660b207a
Guilherme Guerreiro <guiguerreiro10@hotmail.com>
2020-12-29 18:25:03 +0000

    fix

--- client/src/functions/Rehearsals.js
+++ client/src/functions/Rehearsals.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export function getRehearsal(date) {
-    console.log(date);
     return new Promise((resolve, reject) => {
         axios({
             method: "get",
